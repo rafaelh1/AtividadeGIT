957d84019e2cd38e08ff8a38fa0de8d500e8afb5
Rafael <rafael.henrique@alunos.ifsuldeminas.edu.br>
2020-11-23 13:43:49 -0200

    Início do projeto

--- src/main/App.java
+++ src/main/App.java
@@ -4,6 +4,6 @@ package main;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Rafael Henrique");
     }
 }
