e1ad95093808969834b65d1c6192895034b597a8
Rafael <rafael.henrique@alunos.ifsuldeminas.edu.br>
2020-11-23 20:55:04 -0200

    Início do projeto

--- src/main/John.java
+++ src/main/John.java
@@ -2,6 +2,7 @@ package main;
 
 public class John {
      public static void main(String[] args) {
-        System.out.println("John Allan");  
+        System.out.println("John Allan"); 
+        System.out.println("Rafael Henrique");
 }
 }
