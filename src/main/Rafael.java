3b1bb6ee8a7d8c2ff275cb3121d3ff16308f1e39
Rafael <rafael.henrique@alunos.ifsuldeminas.edu.br>
2020-11-23 21:00:25 -0200

    Adicionando nomes

--- src/main/Rafael.java
+++ src/main/Rafael.java
@@ -4,6 +4,6 @@ package main;
 
 public class Rafael {
     public static void main(String[] args) {
-        System.out.println("Rafael Henrique");
+        System.out.println("Rafael Henrique"); 
     }
 }
